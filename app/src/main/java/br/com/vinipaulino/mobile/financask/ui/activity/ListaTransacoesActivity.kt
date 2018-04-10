15eb6565da279b30858125385d8613488f4159c4
vinypaulino <vinydos3@gmail.com>
2018-04-09 23:19:06 -0300

    Initial commit

---
+++ app/src/main/java/br/com/vinipaulino/mobile/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,16 @@
+package br.com.vinipaulino.mobile.financask.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.com.vinipaulino.mobile.financask.R
+
+/**
+ * Created by vinyp on 09/04/2018.
+ */
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+    }
+}
